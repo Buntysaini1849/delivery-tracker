52c30b49e5cfbe70ec2e89485a48c47e6cd34103
BUNTY <buntysaini1849@gmail.com>
2023-06-14 09:55:38 +0530

    delivery-tracking-system

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -574,13 +574,13 @@ export default function Dashboard() {
                       <h6 className="cb-font">Location</h6>
                     </div>
                     <div className="card-body py-2 px-0">
-                      <MapComponent
+                      {/* <MapComponent
                         isMarkerShown
                         googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
                         loadingElement={<div style={{ height: "100%" }} />}
                         containerElement={<div style={{ height: "400px" }} />}
                         mapElement={<div style={{ height: "100%" }} />}
-                      />
+                      /> */}
                     </div>
                   </div>
                 </div>
